df8be27008eac94c2b8b297d31c42933cf295a69
SoHamDongare <Sohamdongare45@gmail.com>
2026-04-19 20:11:15 +0530

    Hardcode Cloudinary for testing

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -135,9 +135,9 @@ import cloudinary.uploader
 import cloudinary.api
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': os.getenv('dlukbq4xu'),
+    'API_KEY': os.getenv('454575889249322'),
+    'API_SECRET': os.getenv('u_KnNbiKRD_OrfLdDNYU5Cj_4bk'),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
