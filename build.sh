8936185a1d3c43601ad0c2e620324b33bcff36b8
SoHamDongare <Sohamdongare45@gmail.com>
2026-05-21 00:30:49 +0530

    remove create_superuser root file

--- build.sh
+++ build.sh
@@ -5,14 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py shell -c "
-import os
-from django.contrib.auth import get_user_model
-U = get_user_model()
-if not U.objects.filter(username=os.environ.get('DJANGO_SUPERUSER_USERNAME')).exists():
-    U.objects.create_superuser(
-        os.environ.get('DJANGO_SUPERUSER_USERNAME'),
-        os.environ.get('DJANGO_SUPERUSER_EMAIL'),
-        os.environ.get('DJANGO_SUPERUSER_PASSWORD')
-    )
-""
+python manage.py shell -c "from django.contrib.auth import get_user_model; U = get_user_model(); U.objects.filter(username='admin').exists() or U.objects.create_superuser('admin', 'codeflowsolutions.dev@gmail.com', 'admin0204')"
